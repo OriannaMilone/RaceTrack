5be5acdf55bfc6045a92dc280a0876f5cba63cef
Orianna Milone <oriannamilone.s@gmail.com>
2025-04-20 17:45:03 +0200

    more races CSVs ADDED

--- fastf1API_data.py
+++ fastf1API_data.py
@@ -62,8 +62,8 @@ def get_pitstops_data():
         pit_stops.to_csv(filename, index=False)
         print(f"Archivo guardado: {filename}")
 
-def get_full_race_data(race, season, session_type='H'):
-    session = ff1.get_session(season, 'Belgian Grand Prix', 'R') 
+def get_full_race_data(race_prefix, race_name, GP_name, season, session_type='H'):
+    session = ff1.get_session(season, GP_name, 'R') 
     session.load()
     
     laps = session.laps
@@ -96,9 +96,65 @@ def get_full_race_data(race, season, session_type='H'):
     merged_data = merged_data.merge(race_status, on='Driver')
 
     merged_data['Season'] = season
-    merged_data['RaceName'] = 'GP Belgica' 
+    merged_data['RaceName'] = race_name 
     merged_data['RaceType'] = 'historica'
 
-    filename = f'{race}_{season}_full_{session_type}_data.csv'
+    # Guardar archivo CSV
+    filename = f'{race_prefix}_{season}_full_{session_type}_data.csv'
     merged_data.to_csv(filename, index=False)
     print(f"Archivo guardado: {filename}")
+
+def get_full_races_data(season, session_type='H'):
+    session = ff1.get_session(season, 'Belgian Grand Prix', 'R') 
+    session.load()
+    
+    laps = session.laps
+    results = session.results
+    
+    lap_data = laps[['LapNumber', 'Driver', 'DriverNumber', 'Team', 'Position', 'LapTime', 'IsPersonalBest']]
+    lap_data_sectors = laps[['LapNumber', 'Driver', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'Compound']]
+    laps_data = lap_data.merge(lap_data_sectors, on=['LapNumber', 'Driver'])
+
+    pit_in_laps = laps[laps['PitInTime'].notnull()][['LapNumber', 'Driver', 'PitInTime']].rename(columns={'LapNumber': 'LapNumber_In'})
+    pit_out_laps = laps[laps['PitOutTime'].notnull()][['LapNumber', 'Driver', 'PitOutTime']].rename(columns={'LapNumber': 'LapNumber_Out'})
+
+    pit_stops = pd.merge(pit_in_laps, pit_out_laps, on='Driver', suffixes=('_In', '_Out'))
+    pit_stops = pit_stops[pit_stops['LapNumber_Out'] == pit_stops['LapNumber_In'] + 1]
+    pit_stops['PitStopDuration'] = pit_stops['PitOutTime'] - pit_stops['PitInTime']
+
+    merged_data = laps_data.merge(pit_stops[['LapNumber_In', 'Driver', 'PitInTime', 'PitOutTime', 'PitStopDuration']], 
+                                  how='left', left_on=['LapNumber', 'Driver'], right_on=['LapNumber_In', 'Driver'])
+
+    merged_data.drop(columns=['LapNumber_In'], inplace=True)
+
+    race_status = results[["Abbreviation", "Position", "GridPosition", "Status"]].rename(columns={
+                "Abbreviation": "Driver",
+                "Position": "FinishingPosition",
+                "Status": "FinalStatus"
+            })
+
+    merged_data = merged_data.merge(race_status, on='Driver')
+    merged_data['Season'] = season
+    merged_data['RaceName'] = 'GP Belgica'
+    merged_data['RaceType'] = 'historica'
+
+    return merged_data
+
+# all_years_data = []
+
+# for season in range(2018, 2025):  
+#     try:
+#         data = get_full_races_data(season)
+#         all_years_data.append(data)
+#     except Exception as e:
+#         print(f"Error en la temporada {season}: {e}")
+
+# df_final = pd.concat(all_years_data, ignore_index=True)
+# df_final.to_csv('SPA_2018_2025_full_H_data.csv', index=False)
+# print("Archivo guardado: SPA_2018_2025_full_H_data.csv")
+
+# GPs = ["Brazilian Grand Prix", "Italian Grand Prix", "Belgian Grand Prix", "Monaco Grand Prix"]
+
+for season in range(2018, 2025):
+    get_full_race_data('MONZA', 'GP Monza', 'Italian Grand Prix', season)
+    get_full_race_data('SAOPAULO', 'GP São Paulo', 'Brazilian Grand Prix', season)
