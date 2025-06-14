480851c46a224a4d836bf79891f62f60992a8b92
Orianna Milone <oriannamilone.s@gmail.com>
2025-06-14 14:28:22 +0200

    Refactor ddbb system

---
+++ database/db_load_data.py
@@ -0,0 +1,69 @@
+from db_operations import *
+
+if __name__ == "__main__":
+    # Years to process
+    YEARS = range(2018, 2025)
+
+    # SPA DATA -------------------------------------------------------------
+    SPA_BASE_PATH = "../SPA_DATA/"
+    SPA_PATHS = {
+        "drivers": "drivers_data/Spa_{year}_drivers_info.csv",
+        "drivers_teams": "drivers_teams/Spa_{year}_drivers_teams_info.csv",
+        "race_status": "race_status/Spa_{year}_race_status.csv",
+        "race_laps": "spa_race_results/Spa_{year}_race_laps.csv",
+        "pitstops": "pitstops/Spa_{year}_pitstops_data.csv",
+        "full_data": "full_data_race/SPA_{year}_full_H_data.csv"
+    }
+
+    # Insert drivers
+    drivers_files = [SPA_BASE_PATH + SPA_PATHS["drivers"].format(year=year) for year in YEARS]
+    insert_drivers_from_csv(drivers_files)
+
+    # Insert teams
+    teams_files = [SPA_BASE_PATH + SPA_PATHS["drivers_teams"].format(year=year) for year in YEARS]
+    insert_teams_from_csv(teams_files)
+
+    for year in YEARS:
+        drivers_teams_file = SPA_BASE_PATH + SPA_PATHS["drivers_teams"].format(year=year)
+        insert_driver_team_participation(drivers_teams_file, year)
+
+        insert_race(year, "GP Belgica", "Spa-Francorchamps", "historica")
+        insert_race(year, "GP Monaco", "Circuito de Montecarlo", "historica")
+        insert_race(year, "GP Monza", "Autodromo Nazionale Monza", "historica")
+        insert_race(year, "GP Sao Paulo", "Autódromo José Carlos Pace", "historica")
+        
+        race_status_file = SPA_BASE_PATH + SPA_PATHS["race_status"].format(year=year)
+        insert_race_status_from_csv(race_status_file, year)
+
+        race_laps_file = SPA_BASE_PATH + SPA_PATHS["race_laps"].format(year=year)
+        insert_laps_from_csv(race_laps_file, year)
+
+        pitstops_file = SPA_BASE_PATH + SPA_PATHS["pitstops"].format(year=year)
+        insert_pitstops_from_csv(pitstops_file)
+
+        full_data_file = SPA_BASE_PATH + SPA_PATHS["full_data"].format(year=year)
+        insert_full_race_data(full_data_file)
+
+    # CIRCUITS FULL DATA ---------------------------------------------------
+    BASE_PATHS = {
+        "monaco": "../MONACO_DATA/",
+        "monza": "../MONZA_DATA/",
+        "saopaulo": "../SAOPAULO_DATA/"
+    }
+
+    CIRCUIT_PREFIXES = {
+        "monaco": "MONACO",
+        "monza": "MONZA",
+        "saopaulo": "SAOPAULO"
+    }
+
+    FULL_DATA_TEMPLATE = "full_data_race/{prefix}_{year}_full_H_data.csv"
+
+    for circuit_key, prefix in CIRCUIT_PREFIXES.items():
+        base_path = BASE_PATHS[circuit_key]
+        for year in YEARS:
+            file_path = base_path + FULL_DATA_TEMPLATE.format(prefix=prefix, year=year)
+            insert_full_race_data(file_path)
+
+        
+    print("Away we go!")
--- database/db_operations.py
+++ database/db_operations.py
@@ -3,7 +3,7 @@ import pandas as pd
 import uuid
 
 def format_timedelta(td):
-    """Convierte Timedelta a mm:ss.SSS"""
+    """Converts Timedelta to mm:ss.SSS format."""
     if pd.isna(td):
         return None
     total_seconds = td.total_seconds()
@@ -11,6 +11,7 @@ def format_timedelta(td):
     return f"{int(minutes):02}:{seconds:06.3f}"
 
 def format_timedelta2(td):
+    """Converts Timedelta to hh:mm:ss.SSS format."""
     if pd.isna(td):
         return None  
     total_seconds = td.total_seconds()
@@ -18,7 +19,7 @@ def format_timedelta2(td):
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours):02}:{int(minutes):02}:{seconds:06.3f}"
 
-def insertar_pilotos_csv(csv_files):
+def insert_drivers_from_csv(csv_files):
     conn = get_connection()
     if not conn:
         return
@@ -26,27 +27,25 @@ def insertar_pilotos_csv(csv_files):
     try:
         cursor = conn.cursor()
         
-        dfs = [pd.read_csv(archivo) for archivo in csv_files]
-        df_final = pd.concat(dfs, ignore_index=True).drop_duplicates()
+        dfs = [pd.read_csv(file) for file in csv_files]
+        final_df = pd.concat(dfs, ignore_index=True).drop_duplicates()
 
-        print(df_final)  
-
-        for _, row in df_final.iterrows():
+        for _, row in final_df.iterrows():
             cursor.execute(
                 "INSERT INTO Piloto (id, nombre, numerocompeticion) VALUES (%s, %s, %s)",
                 (str(uuid.uuid4()), row["Driver"], row["DriverNumber"])
             )
 
         conn.commit()
-        print("Datos de pilotos insertados correctamente.")
+        print("Drivers inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting drivers:", e)
     finally:
         cursor.close()
         conn.close()
 
-def insertar_equipos_csv(csv_files):
+def insert_teams_from_csv(csv_files):
     conn = get_connection()
     if not conn:
         return
@@ -54,26 +53,25 @@ def insertar_equipos_csv(csv_files):
     try:
         cursor = conn.cursor()
         
-        dfs = [pd.read_csv(archivo) for archivo in csv_files]
-        df_final = pd.concat(dfs, ignore_index=True)
-        df_final = df_final.iloc[:, 1:]
-        df_final = df_final.drop_duplicates(subset=["Team"])
+        dfs = [pd.read_csv(file) for file in csv_files]
+        final_df = pd.concat(dfs, ignore_index=True)
+        final_df = final_df.iloc[:, 1:]
+        final_df = final_df.drop_duplicates(subset=["Team"])
 
-        print(df_final)  
-
-        for _, row in df_final.iterrows():
+        for _, row in final_df.iterrows():
             cursor.execute(
                 "INSERT INTO Equipo (id, nombreescuderia) VALUES (%s, %s)",
                 (str(uuid.uuid4()), row["Team"]))
+        
         conn.commit()
-        print("Datos de equipos insertados correctamente.")    
+        print("Teams inserted successfully.")    
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting teams:", e)
     finally:
         cursor.close()
         conn.close()
-            
-def insertar_pilotos_equipos_csv(csv_file, season):
+
+def insert_driver_team_participation(csv_file, season):
     conn = get_connection()
     if not conn:
         return
@@ -82,8 +80,7 @@ def insertar_pilotos_equipos_csv(csv_file, season):
         cursor = conn.cursor()
         
         df = pd.read_csv(csv_file)
-        print(df)  
-
+        
         drivers_ids = []
         teams_ids = []
         
@@ -99,22 +96,23 @@ def insertar_pilotos_equipos_csv(csv_file, season):
                 teams_ids.append(team_id[0])
                 
         if len(drivers_ids) != len(teams_ids):
-            return "Error en la obtención de ids de pilotos y equipos."
+            return "Error matching drivers and teams IDs."
+        
         for i in range(len(drivers_ids)):
-            cursor.execute(" INSERT INTO ParticipacionEquipo (id, id_piloto, id_equipo, temporada) VALUES (%s, %s, %s, %s)",
+            cursor.execute("INSERT INTO ParticipacionEquipo (id, id_piloto, id_equipo, temporada) VALUES (%s, %s, %s, %s)",
                 (str(uuid.uuid4()), drivers_ids[i], teams_ids[i], season)
             )
 
         conn.commit()
-        print("Datos de pilotos y equipos insertados correctamente.")
+        print("Driver-Team participations inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting participations:", e)
     finally:
         cursor.close()
         conn.close()  
         
-def insertar_carrera_datos(year, name, circuit, type):
+def insert_race(year, name, circuit, race_type):
     conn = get_connection()
     if not conn:
         return
@@ -124,37 +122,32 @@ def insertar_carrera_datos(year, name, circuit, type):
         
         cursor.execute(
             "INSERT INTO Carrera (id, nombre, circuito, tipo, temporada) VALUES (%s, %s, %s, %s, %s)",
-            (str(uuid.uuid4()), name, circuit, type, year)
+            (str(uuid.uuid4()), name, circuit, race_type, year)
         )
 
         conn.commit()
-        print("Datos de carrera insertados correctamente.")
+        print("Race inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting race:", e)
     finally:
         cursor.close()
-        conn.close()
-    
-def insertar_estado_carrera_csv(csv_file, year):
+        conn.close()        
+
+def insert_race_status_from_csv(csv_file, year):
     conn = get_connection()
     if not conn:
         return
 
     try:
         cursor = conn.cursor()
-        
         df = pd.read_csv(csv_file)
 
         df["FisingPosition"] = df["FisingPosition"].astype(int)
         df["GridPosition"] = df["GridPosition"].astype(int)
         
-        print(df)
-        
         cursor.execute("SELECT id FROM Carrera WHERE temporada = %s", (year,))
-        race_id = cursor.fetchone()
-
-        race_id = race_id[0]
+        race_id = cursor.fetchone()[0]
 
         drivers_ids = []
 
@@ -164,53 +157,44 @@ def insertar_estado_carrera_csv(csv_file, year):
             if driver_id:
                 drivers_ids.append(driver_id[0])
 
-
         for driver_id, (_, row) in zip(drivers_ids, df.iterrows()):
-            cursor.execute(
-                """INSERT INTO ParticipacionCarrera (id_piloto, id_carrera, estado, posicioninicio, posicionfinal) 
-                VALUES (%s, %s, %s, %s, %s)""",
+            cursor.execute("INSERT INTO ParticipacionCarrera (id_piloto, id_carrera, estado, posicioninicio, posicionfinal) VALUES (%s, %s, %s, %s, %s)",
                 (driver_id, race_id, row["FinialStatus"], row["GridPosition"], row["FisingPosition"])
             )
 
         conn.commit()
-        print("Datos de estado de carrera insertados correctamente.")
+        print("Race status inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting race status:", e)
     finally:
         cursor.close()
         conn.close()
+           
+
+        cursor.close()
+        conn.close()
         
-def insertar_vueltas_csv(csv_file, year):
+def insert_laps_from_csv(csv_file, year):
     conn = get_connection()
     if not conn:
         return
 
     try:
         cursor = conn.cursor()
-        
         df = pd.read_csv(csv_file)
-        
-        df = df.drop(columns=["Team"])
-        df = df.drop(columns=["TyreLife"])
+        df = df.drop(columns=["Team", "TyreLife"])
         df["LapNumber"] = df["LapNumber"].astype(int)
         df["Position"] = df["Position"].fillna(0).astype(int)
-        df["Compound"] = df["Compound"].fillna("TBD") #TBD --> To be defined
-        
-        df["LapTime"] = pd.to_timedelta(df["LapTime"], errors="coerce")
-        df["Sector1Time"] = pd.to_timedelta(df["Sector1Time"], errors="coerce")
-        df["Sector2Time"] = pd.to_timedelta(df["Sector2Time"], errors="coerce")
-        df["Sector3Time"] = pd.to_timedelta(df["Sector3Time"], errors="coerce")
+        df["Compound"] = df["Compound"].fillna("TBD")
 
-        df["LapTime"] = df["LapTime"].apply(format_timedelta)
-        df["Sector1Time"] = df["Sector1Time"].apply(format_timedelta)
-        df["Sector2Time"] = df["Sector2Time"].apply(format_timedelta)
-        df["Sector3Time"] = df["Sector3Time"].apply(format_timedelta)
-        
-        print(df)
+        df["LapTime"] = pd.to_timedelta(df["LapTime"], errors="coerce").apply(format_timedelta)
+        df["Sector1Time"] = pd.to_timedelta(df["Sector1Time"], errors="coerce").apply(format_timedelta)
+        df["Sector2Time"] = pd.to_timedelta(df["Sector2Time"], errors="coerce").apply(format_timedelta)
+        df["Sector3Time"] = pd.to_timedelta(df["Sector3Time"], errors="coerce").apply(format_timedelta)
         
         cursor.execute("SELECT id FROM Carrera WHERE temporada = %s", (year,))
-        race_id = cursor.fetchone()
+        race_id = cursor.fetchone()[0]
 
         drivers_ids = []
 
@@ -220,100 +204,78 @@ def insertar_vueltas_csv(csv_file, year):
             if driver_id:
                 drivers_ids.append(driver_id[0])
 
-
         for driver_id, (_, row) in zip(drivers_ids, df.iterrows()):
-            cursor.execute(
-                """INSERT INTO vuelta (id, id_piloto, id_carrera, numerovuelta, posicion, tiempovuelta, sector1tiempo, sector2tiempo, sector3tiempo, compuestoneumatico, mejorvueltapersonal) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+            cursor.execute("""
+                INSERT INTO vuelta (id, id_piloto, id_carrera, numerovuelta, posicion, tiempovuelta, sector1tiempo, sector2tiempo, sector3tiempo, compuestoneumatico, mejorvueltapersonal) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """,
                 (str(uuid.uuid4()), driver_id, race_id, row["LapNumber"], row["Position"], row["LapTime"], row["Sector1Time"], row["Sector2Time"], row["Sector3Time"], row["Compound"], row["IsPersonalBest"])
             )
 
         conn.commit()
-        print("Datos de vuelta insertados correctamente.")
+        print("Laps inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
+        print("Error inserting laps:", e)
     finally:
         cursor.close()
         conn.close()
-        
-def insertar_paradas_csv(csv_file):
+
+def insert_pitstops_from_csv(csv_file):
     conn = get_connection()
     if not conn:
         return
 
     try:
         cursor = conn.cursor()
-
         df = pd.read_csv(csv_file)
 
-        df["PitInTime"] = pd.to_timedelta(df["PitInTime"], errors="coerce")
-        df["PitOutTime"] = pd.to_timedelta(df["PitOutTime"], errors="coerce")
-        df["PitStopDuration"] = pd.to_timedelta(df["PitStopDuration"], errors="coerce")
+        df["PitInTime"] = pd.to_timedelta(df["PitInTime"], errors="coerce").apply(format_timedelta2)
+        df["PitOutTime"] = pd.to_timedelta(df["PitOutTime"], errors="coerce").apply(format_timedelta2)
+        df["PitStopDuration"] = pd.to_timedelta(df["PitStopDuration"], errors="coerce").apply(format_timedelta2)
 
-        df["PitInTime"] = df["PitInTime"].apply(format_timedelta2)
-        df["PitOutTime"] = df["PitOutTime"].apply(format_timedelta2)
-        df["PitStopDuration"] = df["PitStopDuration"].apply(format_timedelta2)
-        
         for _, row in df.iterrows():
             cursor.execute("SELECT id FROM Carrera WHERE temporada = %s", (row["Season"],))
-            carrera_id = cursor.fetchone()
-
-            if not carrera_id:
-                print(f"⚠ Advertencia: No se encontró la carrera para la temporada {row['Season']}")
+            race_id = cursor.fetchone()
+            if not race_id:
+                print(f"Warning: Race not found for season {row['Season']}")
                 continue
-
-            carrera_id = carrera_id[0]
+            race_id = race_id[0]
 
             cursor.execute("SELECT id FROM Piloto WHERE nombre = %s", (row["Driver"],))
-            piloto_id = cursor.fetchone()
+            driver_id = cursor.fetchone()[0]
 
-            piloto_id = piloto_id[0]  
+            cursor.execute("SELECT id FROM Vuelta WHERE id_piloto = %s AND id_carrera = %s AND numerovuelta = %s", (driver_id, race_id, row["LapNumber_In"]))
+            lap_in_id = cursor.fetchone()[0]
 
-            cursor.execute("""
-                SELECT id FROM Vuelta 
-                WHERE id_piloto = %s AND id_carrera = %s AND numerovuelta = %s
-            """, (piloto_id, carrera_id, row["LapNumber_In"]))
-            vuelta_entrada_id = cursor.fetchone()
-
-            cursor.execute("""
-                SELECT id FROM Vuelta 
-                WHERE id_piloto = %s AND id_carrera = %s AND numerovuelta = %s
-            """, (piloto_id, carrera_id, row["LapNumber_Out"]))
-            vuelta_salida_id = cursor.fetchone()
-
-            vuelta_entrada_id = vuelta_entrada_id[0]
-            vuelta_salida_id = vuelta_salida_id[0]
+            cursor.execute("SELECT id FROM Vuelta WHERE id_piloto = %s AND id_carrera = %s AND numerovuelta = %s", (driver_id, race_id, row["LapNumber_Out"]))
+            lap_out_id = cursor.fetchone()[0]
 
             cursor.execute("""
                 INSERT INTO paradaboxes (id, id_piloto, id_carrera, id_vuelta_entra, id_vuelta_sale, tiempoentrada, tiemposalida, duracionparada, tipoparada)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, (
-                str(uuid.uuid4()), piloto_id, carrera_id, vuelta_entrada_id, vuelta_salida_id,
-                row["PitInTime"], row["PitOutTime"], row["PitStopDuration"], "Cambio de neumáticos"  # Puedes ajustar el tipo de parada si es necesario
-            ))
+            """,
+                (str(uuid.uuid4()), driver_id, race_id, lap_in_id, lap_out_id, row["PitInTime"], row["PitOutTime"], row["PitStopDuration"], "Cambio de neumáticos")
+            )
 
         conn.commit()
-        print("Datos de paradas en boxes insertados correctamente.")
+        print("Pitstops inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
-
+        print("Error inserting pitstops:", e)
     finally:
         cursor.close()
         conn.close()
 
-def insertar_datos_full_carrera(csv_file):
+def insert_full_race_data(csv_file):
     conn = get_connection()
     if not conn:
         return
 
     try:
         cursor = conn.cursor()
-
         df = pd.read_csv(csv_file)
 
-        # Convertir tiempos a INTERVAL
         df["LapTime"] = pd.to_timedelta(df["LapTime"], errors="coerce").apply(format_timedelta)
         df["Sector1Time"] = pd.to_timedelta(df["Sector1Time"], errors="coerce").apply(format_timedelta)
         df["Sector2Time"] = pd.to_timedelta(df["Sector2Time"], errors="coerce").apply(format_timedelta)
@@ -322,7 +284,6 @@ def insertar_datos_full_carrera(csv_file):
         df["PitOutTime"] = pd.to_timedelta(df["PitOutTime"], errors="coerce").apply(format_timedelta2)
         df["PitStopDuration"] = pd.to_timedelta(df["PitStopDuration"], errors="coerce").apply(format_timedelta2)
 
-        # Manejo de valores nulos para columnas categóricas
         df["Compound"] = df["Compound"].fillna("TBD")
         df["RaceName"] = df["RaceName"].fillna("GP")  
         df["Season"] = df["Season"].fillna(0).astype(int)  
@@ -336,8 +297,7 @@ def insertar_datos_full_carrera(csv_file):
 
         for _, row in df.iterrows():
             try:
-                    # print("Intentando insertar fila:", row.to_dict())
-                    cursor.execute("""
+                cursor.execute("""
                     INSERT INTO fullDatosCarreras (
                         id, numerovuelta, piloto, numeropiloto, equipo, posicion, tiempovuelta, mejorvueltapersonal, 
                         sector1tiempo, sector2tiempo, sector3tiempo, compuestoneumatico, pittiempoentrada, 
@@ -345,23 +305,21 @@ def insertar_datos_full_carrera(csv_file):
                         posicioninicio, estadofinalcarrera, tipocarrera
                     ) 
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """, (
-                    str(uuid.uuid4()), row["LapNumber"], row["Driver"], row["DriverNumber"], row["Team"], 
-                    row["Position"], row["LapTime"], row["IsPersonalBest"], row["Sector1Time"], row["Sector2Time"], 
-                    row["Sector3Time"], row["Compound"], row["PitInTime"], row["PitOutTime"], row["PitStopDuration"],
-                    row["Season"], row["RaceName"], row["FinishingPosition"], row["GridPosition"], 
-                    row["FinalStatus"], row["RaceType"]
-                ))
+                """,
+                (str(uuid.uuid4()), row["LapNumber"], row["Driver"], row["DriverNumber"], row["Team"], 
+                 row["Position"], row["LapTime"], row["IsPersonalBest"], row["Sector1Time"], row["Sector2Time"], 
+                 row["Sector3Time"], row["Compound"], row["PitInTime"], row["PitOutTime"], row["PitStopDuration"],
+                 row["Season"], row["RaceName"], row["FinishingPosition"], row["GridPosition"], 
+                 row["FinalStatus"], row["RaceType"])
+                )
             except Exception as e:
-                print(f"Error en la fila {row.to_dict()}: {e}")
+                print(f"Error in row {row.to_dict()}: {e}")
         
         conn.commit()
-        print("Datos de carrera completa insertados correctamente.")
+        print("Full race data inserted successfully.")
 
     except Exception as e:
-        print("Error al insertar datos:", e)
-
+        print("Error inserting full race data:", e)
     finally:
         cursor.close()
         conn.close()
-    
